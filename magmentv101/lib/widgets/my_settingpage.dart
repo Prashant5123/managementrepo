bf2d7c27e7ac62cd1b2c60d941517a8103fc44bb
Prashant5123 <gordeprashant5123@gmail.com>
2025-06-11 15:50:41 +0530

    managementapp

---
+++ magmentv101/lib/widgets/my_settingpage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.grey.shade700,
+        actionsIconTheme: IconThemeData(),
+        iconTheme: const IconThemeData(color: Colors.grey),
+        title: const Text(
+          "Setting",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+        leading: GestureDetector(
+          child: Icon(Icons.arrow_back_ios),
+          onTap: () {
+            Navigator.pop(context);
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          boxShadow: [BoxShadow(blurRadius: 5.0)],
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        margin: const EdgeInsets.all(25),
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              "Dark Mode",
+              style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
